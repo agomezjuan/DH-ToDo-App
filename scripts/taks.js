2ad44c390481b5629a650f63088b625de8dc71f8
Alberto Gomez Juan <agomezjuan@hotmail.com>
2022-04-11 19:56:17 +0200

    Obtener lista de tareas

--- scripts/taks.js
+++ scripts/taks.js
@@ -1,3 +1,10 @@
+// Constantes
+const inputTask = document.querySelector("#nuevaTarea");
+const taskCommit = document.querySelector("form.nueva-tarea");
+
+/**
+ * Verificar la sesion del usuario
+ */
 window.addEventListener("load", () => {
   let auth = sessionStorage.getItem("auth");
   if (typeof auth === "undefined") {
@@ -5,20 +12,32 @@ window.addEventListener("load", () => {
     auth = null;
   }
 
-  /** Si existe la llave de autenticación */
+  // Si existe la llave de autenticación */
   if (auth) {
-    /** Obtener información de usuario */
-    getUserData(auth);
+    // Obtener información de usuario */
+    obtenerDatosUsuario(auth);
+    cargartareasViejas(auth);
   } else {
-    /** De lo contrario redirige al inicio */
+    // De lo contrario redirigir al inicio */
     accesoRestringido();
   }
 });
 
 /**
+ * Evento Crear Tarea
+ * */
+taskCommit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let auth = sessionStorage.getItem("auth");
+  crearTarea(auth);
+});
+
+/** -------------------- Funciones ---------------------- */
+
+/**
  * Obtener la información del Usuario
  * */
-function getUserData(auth) {
+function obtenerDatosUsuario(auth) {
   fetch("https://ctd-todo-api.herokuapp.com/v1/users/getMe", {
     method: "GET",
     headers: {
@@ -30,6 +49,68 @@ function getUserData(auth) {
       console.table(data);
       const user = document.querySelector(".user-info p");
       user.innerHTML = `ID: ${data.id} - ${data.firstName} ${data.lastName}`;
+    });
+}
+
+/**
+ * Obtener lista de tareas
+ * */
+async function cargartareasViejas(auth) {
+  await fetch("https://ctd-todo-api.herokuapp.com/v1/tasks", {
+    method: "GET",
+    headers: {
+      Authorization: auth,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.table(data);
+      const taskBlock = document.querySelector("#skeleton");
+
+      data.forEach((tarea) => {
+        const taskTemplate = `<li data-aos="fade-up" class="tarea">
+            <div class="not-done"></div>
+            <div class="descripcion">
+            <p class="nombre">${tarea.description}</p>
+            <p class="timestamp">Creada: ${dayjs(tarea.createdAt)}</p>
+            </div>
+            </li>`;
+
+        taskBlock.insertAdjacentHTML("afterbegin", taskTemplate);
+      });
+    });
+}
+
+/**
+ * Crear una tarea nueva
+ * */
+async function crearTarea(auth) {
+  console.log(inputTask.value);
+  await fetch("https://ctd-todo-api.herokuapp.com/v1/tasks", {
+    method: "POST",
+    body: JSON.stringify({
+      description: inputTask.value,
+      completed: false,
+    }),
+    headers: {
+      Authorization: auth,
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.table(data);
+
+      const taskTemplate = `<li data-aos="fade-up" class="tarea">
+        <div class="not-done"></div>
+        <div class="descripcion">
+          <p class="nombre">${data.description}</p>
+          <p class="timestamp">Creada: ${dayjs(data.createdAt)}</p>
+        </div>
+      </li>`;
+
+      const taskBlock = document.querySelector("#skeleton");
+      taskBlock.insertAdjacentHTML("afterbegin", taskTemplate);
     });
 }
 
