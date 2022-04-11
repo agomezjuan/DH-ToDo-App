228c3ce4a9fba642e42da9f7ad31379d840fa970
Alberto Gomez Juan <agomezjuan@hotmail.com>
2022-04-11 18:22:35 +0200

    refactor Creacion de funciones de registro

--- scripts/signup.js
+++ scripts/signup.js
@@ -7,61 +7,47 @@ const inputPassword = document.querySelector("#inputPassword");
 const checkPassword = document.querySelector("#checkPassword");
 
 checkPassword.addEventListener("keyup", (e) => {
-  passwordMatch();
+  passwordMatch(inputPassword, checkPassword);
 });
 
 signupForm.addEventListener("submit", (event) => {
   event.preventDefault();
   if (signupValidation()) {
-    fetch("https://ctd-todo-api.herokuapp.com/v1/users", {
-      method: "POST",
-      body: JSON.stringify(
-        normalize(
-          inputNombre.value,
-          inputApellido.value,
-          inputEmail.value,
-          inputPassword.value
-        )
-      ),
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.jwt) {
-          console.log(data.jwt);
-          let timerInterval;
-          Swal.fire({
-            icon: "success",
-            title: "Registro exitoso!",
-            timer: 2000,
-            didOpen: () => {
-              Swal.showLoading();
-              const b = Swal.getHtmlContainer().querySelector("b");
-              timerInterval = setInterval(() => {
-                b.textContent = Swal.getTimerLeft();
-              }, 100);
-            },
-            willClose: () => {
-              clearInterval(timerInterval);
-            },
-          }).then((result) => {
-            /* Read more about handling dismissals below */
-            if (result.dismiss === Swal.DismissReason.timer) {
-              sessionStorage.setItem("auth", data.jwt);
-              window.location.href = "/mis-tareas.html";
-            }
-          });
-        }
-
-        // Utilizamos la informacion para mostrar el mensaje en pantalla
-      });
+    registrarUsuario();
   }
 });
 
-/** Normalizar datos */
+/**
+ * Registrar nuevo usuario
+ */
+function registrarUsuario() {
+  fetch("https://ctd-todo-api.herokuapp.com/v1/users", {
+    method: "POST",
+    body: JSON.stringify(
+      normalize(
+        inputNombre.value,
+        inputApellido.value,
+        inputEmail.value,
+        inputPassword.value
+      )
+    ),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data.jwt) {
+        console.log(data.jwt);
+        registroExitoso(data);
+      }
+    });
+}
+
+/**
+ * Normalizar datos
+ * */
 function normalize(name, lastname, email, password) {
   const data = {
     firstName: name[0].toUpperCase() + name.slice(1).toLowerCase(),
@@ -70,4 +56,33 @@ function normalize(name, lastname, email, password) {
     password: password.trim(),
   };
   return data;
+}
+
+/**
+ * Mensajes de alerta
+ * */
+// Registro realizado con éxito
+function registroExitoso(data) {
+  let timerInterval;
+  Swal.fire({
+    icon: "success",
+    title: "Registro exitoso!",
+    timer: 2000,
+    didOpen: () => {
+      Swal.showLoading();
+      const b = Swal.getHtmlContainer().querySelector("b");
+      timerInterval = setInterval(() => {
+        b.textContent = Swal.getTimerLeft();
+      }, 100);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+    },
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      sessionStorage.setItem("auth", data.jwt);
+      window.location.href = "/mis-tareas.html";
+    }
+  });
 }
